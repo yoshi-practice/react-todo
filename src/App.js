45e328087dcde017f2cb95dd36ed6c4a370bfa5e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-04 08:54:05 +0900

    [Update] ToDoListItem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDoListItem from "./ToDoListItem/ToDoListItem.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello, React!</code>
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <ToDoListItem
+            title="ホームページ作成"
+            description="サークルホームページを今週中に作成する"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
